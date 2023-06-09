68ceb22ab624f73949f824c38260973c2d46c852
murilognc <murilognc@hotmail.com.br>
2023-06-09 11:11:51 -0400

    Fix to deploy prod

--- src/app/api.js
+++ src/app/api.js
@@ -1,6 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
+
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: "http://popmodeler.ledes.net/api/",
+  timeout: 300000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
