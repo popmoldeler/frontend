c508cfc3e5936ba76d337ae13a9ca62c60ab9c97
Sidny Molina <sidny.molina@gmail.com>
2023-11-25 10:55:34 -0300

    Ignorar outras tarefas com erro, Ajuste-AcaoTextualIngles, Ajuste nos campos da Dialog

--- src/app/components/reliabilityRequirementsExtractEnglish/reliabilityInfosToTextEnglish.jsx
+++ src/app/components/reliabilityRequirementsExtractEnglish/reliabilityInfosToTextEnglish.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 
-function criarTextoAcaoEnglish(constituinte_origem, constituinte_destino, momento_falha, falha, solucao_falha) {
+function criarTextoAcaoEnglish(constituinte_origem, constituinte_destino, momento_falha, tipo_interacao, falha, solucao_falha) {
     const combinedArray = falha.map((fail, index) => {
         return fail + ", then " + solucao_falha[index];
       });
-    return `During the interoperability of ${constituinte_origem} to the ${constituinte_destino}, when ${momento_falha} upon occurrence ${combinedArray.join(" ")}`;
+
+      let tipoAcao = '';
+      if (tipo_interacao === 'envio') {
+          tipoAcao = 'sending message';
+      } else if (tipo_interacao === 'recebimento') {
+          tipoAcao = 'receiving message';
+      }
+
+    return `During the ${tipoAcao} of ${constituinte_origem} to the ${constituinte_destino}, when ${momento_falha} upon occurrence ${combinedArray.join(" ")}`;
 }
 
 export {
