d9900b2ec2fa79b4a93f41788992a3602eedf085
Sidny Molina <sidny.molina@gmail.com>
2023-11-25 10:04:10 -0300

    Ajuste-AcaoTextual + Ajuste na Dialog

--- src/app/components/reliabilityRequirementsExtract/reliabilityInfosToText.jsx
+++ src/app/components/reliabilityRequirementsExtract/reliabilityInfosToText.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 // Ação textual 
-function criarTextoAcao(constituinte_origem, constituinte_destino, momento_falha, falha, solucao_falha) {
+function criarTextoAcao(constituinte_origem, constituinte_destino, momento_falha, tipo_interacao, falha, solucao_falha) {
     const combinedArray = falha.map((fail, index) => {
         return fail + ", então " + solucao_falha[index];
       });
-    return `Durante a interoperabilidade do ${constituinte_origem} para o ${constituinte_destino}, quando ${momento_falha} ao ocorrer ${combinedArray.join(" ")}`;
+
+      let tipoAcao = '';
+      if (tipo_interacao === 'envio') {
+          tipoAcao = 'envio de mensagem';
+      } else if (tipo_interacao === 'recebimento') {
+          tipoAcao = 'recebimento de mensagem';
+      }
+  
+    return `Durante o ${tipoAcao} do ${constituinte_origem} para o ${constituinte_destino}, quando ${momento_falha} ao ocorrer ${combinedArray.join(" ")}`;
 }
 
 export {
